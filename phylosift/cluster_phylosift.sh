2a67090cc67378f3bc9b67e67ff3b89ddcf53e88
Stefan Janssen <sjanssen@bioinf002.helmholtz-hzi.de>
2015-12-02 12:24:33 +0100

    changed out dirs to /vol/projects restriction to bioinf002 machine.

--- phylosift/cluster_phylosift.sh
+++ phylosift/cluster_phylosift.sh
@@ -2,11 +2,12 @@
 
 #$ -S /usr/bin/bash
 #$ -N d_phylosift
-#$ -e /home/sjanssen/dockerruns/ERR/
-#$ -o /home/sjanssen/dockerruns/OUT/
+#$ -e /vol/projects/sjanssen/dockerruns/ERR/
+#$ -o /vol/projects/sjanssen/dockerruns/OUT/
 #$ -pe multislot 4
 #$ -cwd
+#$ -l hostname=bioinf002
 
 cd /home/sjanssen/docker_profiling_tools
 docker build -f phylosift/Dockerfile_phylosift  -t phylosift .
-docker run -v "/vol/projects/sjanssen/CAMI/:/exchange/input" -v "/home/sjanssen/dockerruns/phylosift_1:/exchange/output:rw" -t phylosift
+docker run -v "/vol/projects/sjanssen/CAMI/:/exchange/input" -v "/vol/projects/sjanssen/dockerruns/phylosift_2:/exchange/output:rw" -t phylosift
