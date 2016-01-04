ac94ae98e0032656d5adc167ef6913ff55ada37c
Stefan Janssen <sjanssen@bioinf008.helmholtz-hzi.de>
2016-01-04 17:01:22 +0100

    try to prevent "read only file system"

--- metacv/task_metacv.pl
+++ metacv/task_metacv.pl
@@ -22,8 +22,8 @@ foreach my $task (@tasks) {
 		#~ push @{$task->{commands}}, "deinterleave_fastq.sh < <(gunzip -c -d ".$task->{inputfile}.") forward.fastq reverse.fastq";
 		push @{$task->{commands}}, "deinterleave.pl <(gunzip -c -d ".$task->{inputfile}.") /tmp/run/forward.fastq /tmp/run/reverse.fastq";
 		push @{$task->{commands}}, "metacv classify ".$ENV{PREFIX}."/share/metacv_database/cvk6_2059 forward.fastq reverse.fastq output --threads=".$ENV{NCORES};
-		push @{$task->{commands}}, "cp output.res ".$task->{resultfilename}.".orig";
-		push @{$task->{commands}}, "perl -I ".$ENV{PREFIX}."/src/".$ENV{MAPPERNAME}."/ ".$ENV{PREFIX}."/src/".$ENV{MAPPERNAME}."/convert.pl ".$task->{resultfilename}.".orig ".$ENV{PREFIX}."/share/taxonomy/ \"".$task->{inputfile}."\" > ".$task->{resultfilename}.".profile";
+		push @{$task->{commands}}, "cp /tmp/run/output.res ".$task->{resultfilename}.".orig";
+		push @{$task->{commands}}, "perl -I ".$ENV{PREFIX}."/src/".$ENV{MAPPERNAME}."/ ".$ENV{PREFIX}."/src/".$ENV{MAPPERNAME}."/convert.pl /tmp/run/output.res ".$ENV{PREFIX}."/share/taxonomy/ \"".$task->{inputfile}."\" > ".$task->{resultfilename}.".profile";
 	} elsif ($task->{type} eq $Utils::TYPE_CONTIGS) {
 		#call for contig as inputs
 	}
