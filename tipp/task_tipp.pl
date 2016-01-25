b686020860221b664142487f816206341aafd495
sjanssen2 <stefan.janssen@helmholtz-hzi.de>
2016-01-25 17:16:52 +0100

    updates TIPP to YAML

--- tipp/task_tipp.pl
+++ tipp/task_tipp.pl
@@ -1,33 +1,24 @@
 #!/usr/bin/env perl
 
 use lib "../";
-use lib "/biobox/src/IDmapper/";
+use lib "/biobox/lib/";
 
 use strict;
 use warnings;
 use Data::Dumper;
 use Utils;
 
-my @tasks = @{Utils::collectTasks()};
+my @tasks = @{Utils::collectYAMLtasks()};
 foreach my $task (@tasks) {
 	$task->{commands} = [];
 	
-	push @{$task->{commands}}, "rm -rf /tmp/run/";
-	push @{$task->{commands}}, "mkdir -p /tmp/run/";
-	push @{$task->{commands}}, "cd /tmp/run/";
-
-	if ($task->{type} eq $Utils::TYPE_READ_SINGLE) {
-		#call for single end read inputs
-	} elsif ($task->{type} eq $Utils::TYPE_READ_PAIRED) {
-		#call for paired end read inputs
-		push @{$task->{commands}}, "seqtk seq -a ".$task->{inputfile}." > input.fasta";
-	} elsif ($task->{type} eq $Utils::TYPE_CONTIGS) {
-		#call for contig as inputs
-	}
-	
+	push @{$task->{commands}}, "rm -rf ".$task->{cacheDir}."/run/";
+	push @{$task->{commands}}, "mkdir -p ".$task->{cacheDir}."/run/";
+	push @{$task->{commands}}, "cd ".$task->{cacheDir}."/run/";
+	push @{$task->{commands}}, "seqtk seq -a ".$task->{inputfile}." > input.fasta";
 	push @{$task->{commands}}, "run_abundance.py -f input.fasta -c /root/.sepp/tipp.config -o .";
 	push @{$task->{commands}}, "cp abundance.species.csv ".$task->{resultfilename}.".orig";
-	push @{$task->{commands}}, "perl -I ".$ENV{PREFIX}."/src/".$ENV{MAPPERNAME}."/ ".$ENV{PREFIX}."/src/".$ENV{MAPPERNAME}."/convert.pl ".$task->{resultfilename}.".orig ".$ENV{PREFIX}."/share/taxonomy/ \"".$task->{inputfile}."\" > ".$task->{resultfilename}.".profile";
+	push @{$task->{commands}}, "perl -I ".$ENV{PREFIX}."/lib/ ".$ENV{PREFIX}."/src/convert.pl ".$task->{resultfilename}.".orig ".$task->{taxonomyDir}." \"".$task->{inputfile}."\" > ".$task->{resultfilename}.".profile";
 }
 
 Utils::executeTasks(\@tasks);
