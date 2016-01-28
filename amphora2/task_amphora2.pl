8dec62d20cbc1624c912069873b9f0ea3535e5f4
sjanssen2 <stefan.janssen@helmholtz-hzi.de>
2016-01-28 17:50:51 +0100

    update to amphora

--- amphora2/task_amphora2.pl
+++ amphora2/task_amphora2.pl
@@ -1,32 +1,23 @@
 #!/usr/bin/env perl
 
 use lib "../";
-use lib "/biobox/src/IDmapper/";
+use lib "/biobox/lib/";
 
 use strict;
 use warnings;
 use Data::Dumper;
 use Utils;
 
-my @tasks = @{Utils::collectTasks()};
+my @tasks = @{Utils::collectYAMLtasks()};
 foreach my $task (@tasks) {
-	$task->{commands} = [];
+	push @{$task->{commands}}, (
 	
-	if ($task->{type} eq $Utils::TYPE_READ_SINGLE) {
-		#call for single end read inputs
-	} elsif ($task->{type} eq $Utils::TYPE_READ_PAIRED) {
-		#call for paired end read inputs
-	} elsif ($task->{type} eq $Utils::TYPE_CONTIGS) {
-		#call for contig as inputs
-	}
-	
-	push @{$task->{commands}}, "rm -rf /tmp/run";
-	push @{$task->{commands}}, "mkdir -p /tmp/run/";
-	push @{$task->{commands}}, "cd /tmp/run/";
-	push @{$task->{commands}}, "perl ".$ENV{PREFIX}."/src/".$ENV{TOOLNAME}."/Scripts/MarkerScanner.pl -DNA <(gunzip -c ".$task->{inputfile}.")";
-	push @{$task->{commands}}, "perl ".$ENV{PREFIX}."/src/".$ENV{TOOLNAME}."/Scripts/MarkerAlignTrim.pl -WithReference -OutputFormat phylip";
-	push @{$task->{commands}}, "perl ".$ENV{PREFIX}."/src/".$ENV{TOOLNAME}."/Scripts/Phylotyping.pl -CPUs ".$ENV{NCORES}." > ".$task->{resultfilename}.".orig";
-	push @{$task->{commands}}, "perl -I ".$ENV{PREFIX}."/src/".$ENV{MAPPERNAME}."/ ".$ENV{PREFIX}."/src/".$ENV{MAPPERNAME}."/convert.pl ".$task->{resultfilename}.".orig ".$ENV{PREFIX}."/src/".$ENV{TOOLNAME}."/Taxonomy/ \"".$task->{inputfile}."\" > ".$task->{resultfilename}.".profile";
+		"perl ".$ENV{PREFIX}."/src/".$ENV{TOOLNAME}."/Scripts/MarkerScanner.pl -DNA <(gunzip -c ".$task->{inputfile}.")",
+		"perl ".$ENV{PREFIX}."/src/".$ENV{TOOLNAME}."/Scripts/MarkerAlignTrim.pl -WithReference -OutputFormat phylip",
+		"perl ".$ENV{PREFIX}."/src/".$ENV{TOOLNAME}."/Scripts/Phylotyping.pl -CPUs `nproc` > ".$task->{resultfilename}.".orig",
+		"perl -I ".$ENV{PREFIX}."/lib/ ".$ENV{PREFIX}."/bin/convert.pl ".$task->{resultfilename}.".orig ".$ENV{PREFIX}."/src/".$ENV{TOOLNAME}."/Taxonomy/ \"".$task->{inputfile}."\" > ".$task->{resultfilename}.".profile",
+			
+	);
 }
 
 Utils::executeTasks(\@tasks);
