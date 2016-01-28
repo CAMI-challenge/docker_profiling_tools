139ba42b344c0cd57937c2a6a81735a9ee5b7efa
sjanssen2 <stefan.janssen@helmholtz-hzi.de>
2016-01-28 16:52:20 +0100

    update to focus

--- focus/task_focus.pl
+++ focus/task_focus.pl
@@ -1,30 +1,21 @@
 #!/usr/bin/env perl
 
 use lib "../";
-use lib "/biobox/src/IDmapper/";
+use lib "/biobox/lib/";
 
 use strict;
 use warnings;
 use Data::Dumper;
 use Utils;
 
-my @tasks = @{Utils::collectTasks()};
+my @tasks = @{Utils::collectYAMLtasks()};
 foreach my $task (@tasks) {
-	$task->{commands} = [];
-	
-	if ($task->{type} eq $Utils::TYPE_READ_SINGLE) {
-		#call for single end read inputs
-	} elsif ($task->{type} eq $Utils::TYPE_READ_PAIRED) {
-		#call for paired end read inputs
-		push @{$task->{commands}}, "rm -rf /tmp/run/*";
-		push @{$task->{commands}}, "mkdir -p /tmp/run/";
-		push @{$task->{commands}}, "cd /tmp/run/";
-		push @{$task->{commands}}, "python ".$ENV{PREFIX}."/src/".$ENV{TOOLNAME}."/focus_cami.py -q <(seqtk seq -a ".$task->{inputfile}.") -m 0.001 -c bd -k 8  > ".$task->{resultfilename}.".orig";
-		push @{$task->{commands}}, "python2.7 ".$ENV{PREFIX}."/src/".$ENV{TOOLNAME}."/focus2result.py ".$task->{resultfilename}.".orig \"cfk8bd\" ".$task->{inputfile}." > ".$task->{resultfilename}.".profile";
-	} elsif ($task->{type} eq $Utils::TYPE_CONTIGS) {
-		#call for contig as inputs
-	}
-	
+	push @{$task->{commands}}, (
+
+		"python ".$ENV{PREFIX}."/src/".$ENV{TOOLNAME}."/focus_cami.py -q <(seqtk seq -a ".$task->{inputfile}.") -m 0.001 -c bd -k 8  > ".$task->{resultfilename}.".orig",
+		"python2.7 ".$ENV{PREFIX}."/src/".$ENV{TOOLNAME}."/focus2result.py ".$task->{resultfilename}.".orig \"cfk8bd\" ".$task->{inputfile}." > ".$task->{resultfilename}.".profile",
+		
+	);
 }
 
 Utils::executeTasks(\@tasks);
