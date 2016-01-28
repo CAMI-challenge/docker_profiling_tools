b64c39303f00c88fb78848d44c8c5b7bfabfb0a7
sjanssen2 <stefan.janssen@helmholtz-hzi.de>
2016-01-28 18:35:57 +0100

    $ fixed

--- amphora2/task_amphora2.pl
+++ amphora2/task_amphora2.pl
@@ -12,8 +12,8 @@ my @tasks = @{Utils::collectYAMLtasks()};
 foreach my $task (@tasks) {
 	push @{$task->{commands}}, (
 	
-		"gunzip -c ".$task->{inputfile}." > $tmpdir/input.fastq",
-		"perl ".$ENV{PREFIX}."/src/".$ENV{TOOLNAME}."/Scripts/MarkerScanner.pl -DNA $tmpdir/input.fastq",
+		"gunzip -c ".$task->{inputfile}." > \$tmpdir/input.fastq",
+		"perl ".$ENV{PREFIX}."/src/".$ENV{TOOLNAME}."/Scripts/MarkerScanner.pl -DNA \$tmpdir/input.fastq",
 		"perl ".$ENV{PREFIX}."/src/".$ENV{TOOLNAME}."/Scripts/MarkerAlignTrim.pl -WithReference -OutputFormat phylip",
 		"perl ".$ENV{PREFIX}."/src/".$ENV{TOOLNAME}."/Scripts/Phylotyping.pl -CPUs `nproc` > ".$task->{resultfilename}.".orig",
 		"perl -I ".$ENV{PREFIX}."/lib/ ".$ENV{PREFIX}."/bin/convert.pl ".$task->{resultfilename}.".orig ".$task->{taxonomyDir}." \"".$task->{inputfile}."\" > ".$task->{resultfilename}.".profile",
