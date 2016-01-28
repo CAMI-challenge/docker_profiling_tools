4338fd44bc2d7ac227d8bab0af92cf02d1928927
sjanssen2 <stefan.janssen@helmholtz-hzi.de>
2016-01-28 17:16:39 +0100

    searching in the right dir

--- motu/task_motu.pl
+++ motu/task_motu.pl
@@ -13,7 +13,7 @@ foreach my $task (@tasks) {
 	push @{$task->{commands}}, (
 	
 		"perl ".$ENV{PREFIX}."/src/".$ENV{TOOLNAME}."/mOTUs.pl --processors=`nproc` ".$task->{inputfile},
-		"resfile=`find /tmp/ -name \"*insert.mm.dist.among.unique.scaled.taxaid.gz\"`",
+		"resfile=`find \$tmpdir -name \"*insert.mm.dist.among.unique.scaled.taxaid.gz\"`",
 		"gunzip -c -d \$resfile > ".$task->{resultfilename}.".orig",
 		"perl -I ".$ENV{PREFIX}."/lib/ ".$ENV{PREFIX}."/bin/convert.pl ".$task->{resultfilename}.".orig ".$task->{taxonomyDir}." \"".$task->{inputfile}."\" > ".$task->{resultfilename}.".profile",
 	
