9fcc76bf5d11f11b782c2319ef16deb1b68eb64c
sjanssen2 <stefan.janssen@helmholtz-hzi.de>
2016-01-28 18:33:27 +0100

    avoiding <() pattern

--- amphora2/task_amphora2.pl
+++ amphora2/task_amphora2.pl
@@ -12,7 +12,8 @@ my @tasks = @{Utils::collectYAMLtasks()};
 foreach my $task (@tasks) {
 	push @{$task->{commands}}, (
 	
-		"perl ".$ENV{PREFIX}."/src/".$ENV{TOOLNAME}."/Scripts/MarkerScanner.pl -DNA <(gunzip -c ".$task->{inputfile}.")",
+		"gunzip -c ".$task->{inputfile}." > $tmpdir/input.fastq",
+		"perl ".$ENV{PREFIX}."/src/".$ENV{TOOLNAME}."/Scripts/MarkerScanner.pl -DNA $tmpdir/input.fastq",
 		"perl ".$ENV{PREFIX}."/src/".$ENV{TOOLNAME}."/Scripts/MarkerAlignTrim.pl -WithReference -OutputFormat phylip",
 		"perl ".$ENV{PREFIX}."/src/".$ENV{TOOLNAME}."/Scripts/Phylotyping.pl -CPUs `nproc` > ".$task->{resultfilename}.".orig",
 		"perl -I ".$ENV{PREFIX}."/lib/ ".$ENV{PREFIX}."/bin/convert.pl ".$task->{resultfilename}.".orig ".$task->{taxonomyDir}." \"".$task->{inputfile}."\" > ".$task->{resultfilename}.".profile",
