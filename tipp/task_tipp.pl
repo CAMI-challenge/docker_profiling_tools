442b8b5f0f1cff0dbe10e3551bda5a2b5087459f
sjanssen2 <stefan.janssen@helmholtz-hzi.de>
2016-01-28 14:59:18 +0100

    updates for tipp.

--- tipp/task_tipp.pl
+++ tipp/task_tipp.pl
@@ -10,15 +10,14 @@ use Utils;
 
 my @tasks = @{Utils::collectYAMLtasks()};
 foreach my $task (@tasks) {
-	$task->{commands} = [];
+	push @{$task->{commands}}, (
 	
-	push @{$task->{commands}}, "rm -rf ".$task->{cacheDir}."/run/";
-	push @{$task->{commands}}, "mkdir -p ".$task->{cacheDir}."/run/";
-	push @{$task->{commands}}, "cd ".$task->{cacheDir}."/run/";
-	push @{$task->{commands}}, "seqtk seq -a ".$task->{inputfile}." > input.fasta";
-	push @{$task->{commands}}, "run_abundance.py -f input.fasta -c /root/.sepp/tipp.config -o .";
-	push @{$task->{commands}}, "cp abundance.species.csv ".$task->{resultfilename}.".orig";
-	push @{$task->{commands}}, "perl -I ".$ENV{PREFIX}."/lib/ ".$ENV{PREFIX}."/src/convert.pl ".$task->{resultfilename}.".orig ".$task->{taxonomyDir}." \"".$task->{inputfile}."\" > ".$task->{resultfilename}.".profile";
+		"seqtk seq -a ".$task->{inputfile}." > input.fasta",
+		"run_abundance.py -f input.fasta -c /root/.sepp/tipp.config -o .",
+		"cp abundance.species.csv ".$task->{resultfilename}.".orig",
+		"perl -I ".$ENV{PREFIX}."/lib/ ".$ENV{PREFIX}."/src/convert.pl ".$task->{resultfilename}.".orig ".$task->{taxonomyDir}." \"".$task->{inputfile}."\" > ".$task->{resultfilename}.".profile",
+		
+	);
 }
 
 Utils::executeTasks(\@tasks);
