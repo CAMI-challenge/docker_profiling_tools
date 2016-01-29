f3de69288f881c2aaa233511309de473042972e6
sjanssen2 <stefan.janssen@helmholtz-hzi.de>
2016-01-29 09:08:55 +0100

    no need to escape $ if in '' quotes

--- commonkmers/task_commonkmers.pl
+++ commonkmers/task_commonkmers.pl
@@ -12,7 +12,7 @@ my @tasks = @{Utils::collectYAMLtasks(undef, "skipTaxonomyCheck")};
 foreach my $task (@tasks) {
 	push @{$task->{commands}}, (
 	
-		'gunzip -c -d '.$task->{inputfile}.' > \$tmpdir/input.fastq',
+		'gunzip -c -d '.$task->{inputfile}.' > $tmpdir/input.fastq',
 		"julia -p `nproc` /usr/local/sbin/Classify.jl -j /jellyfish/jellyfish-2.2.3/bin/./jellyfish -i \$tmpdir/input.fastq -Q C -d /exchange/db/CommonKmersData/ -k sensitive -o ".$task->{resultfilename}.".profile --normalize -s \"".$task->{inputfile}."\"",
 		
 	);
