9a0fde1ef52b48fd25a5d550a85dd289601c1614
sjanssen2 <stefan.janssen@helmholtz-hzi.de>
2016-02-03 10:18:55 +0100

    also download delnodes.dmp

--- downloadTaxonomy.pl
+++ downloadTaxonomy.pl
@@ -3,7 +3,7 @@
 use strict;
 use warnings;
 
-my ($targetDir) = @ARGV;
+my ($targetDir, $moreFiles) = @ARGV;
 die "Downloads a dump of the taxonomy database from NCBI into the target directory.\nArchive will be extracted and a time stamp added.\nUsage: perl $0 <target dir>\n" if (@ARGV != 1);
 
 system("mkdir -p $targetDir") if (not -d $targetDir);
@@ -11,6 +11,7 @@ print STDERR "downloading taxdump ...";
 system("wget -q -O $targetDir/taxdump.tar.gz ftp://ftp.ncbi.nlm.nih.gov/pub/taxonomy/taxdump.tar.gz");
 print STDERR " done.\nextracting ...";
 system("tar -C $targetDir/ -xzvf $targetDir/taxdump.tar.gz names.dmp nodes.dmp merged.dmp ");
+system("tar -C $targetDir/ -xzvf $targetDir/taxdump.tar.gz delnodes.dmp") if ($moreFiles);
 print STDERR " done.\n";
 system("rm -f $targetDir/taxdump.tar.gz");
 system("date +\"%Y.%m.%d\" > $targetDir/taxdump.tar.gz.date");
