aa3d2ac1848c21b7c2c96507e9d35af46945ea99
sjanssen2 <stefan.janssen@helmholtz-hzi.de>
2016-02-03 21:53:25 +0100

    info when using different taxonomy

--- taxypro/task_taxypro.pl
+++ taxypro/task_taxypro.pl
@@ -10,6 +10,8 @@ use Utils;
 
 my @tasks = @{Utils::collectYAMLtasks(undef, "skipTaxonomyCheck")};
 foreach my $task (@tasks) {
+	print STDERR "warning: taxonomy is hard coded in program, thus pointing to an alternative taxonomy directory will have no effect.\n" if ($task->{taxonomyDir} ne $ENV{PREFIX}.'/share/taxonomy/');
+
 	push @{$task->{commands}}, (
 
 		'/home/Tool/uproc-1.2.0/uproc-dna -o '.$task->{resultfilename}.'.orig -s /home/DB/Pfam27/ /home/DB/model/ '.$task->{inputfile},
