8d28b6669ab77eaf6327024238919d358edc624e
sjanssen2 <stefan.janssen@helmholtz-hzi.de>
2016-02-05 18:35:36 +0100

    explicitly use $tmpdir for output

--- metacv/task_metacv.pl
+++ metacv/task_metacv.pl
@@ -13,7 +13,7 @@ foreach my $task (@tasks) {
 	push @{$task->{commands}}, (
 	
 		"deinterleave.pl <(gunzip -c -d ".$task->{inputfile}.") \$tmpdir/forward.fastq \$tmpdir/reverse.fastq",
-		"metacv classify \"".$task->{databaseDir}."/cvk6_2059\" forward.fastq reverse.fastq output --threads=`nproc`",
+		"metacv classify \"".$task->{databaseDir}."/cvk6_2059\" forward.fastq reverse.fastq \$tmpdir/output --threads=`nproc`",
 		"cp \$tmpdir/output.res ".$task->{resultfilename}.".orig",
 		"perl -I ".$ENV{PREFIX}."/lib/ ".$ENV{PREFIX}."/bin/convert.pl \$tmpdir/output.res ".$task->{taxonomyDir}." \"".$task->{inputfile}."\" > ".$task->{resultfilename}.".profile",
 		
