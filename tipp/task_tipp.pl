edc8181716165f2909105e0974246e029569da19
Fernando Meyer <fernando.meyer@gmail.com>
2018-03-23 16:02:04 +0100

    fix tipp

--- tipp/task_tipp.pl
+++ tipp/task_tipp.pl
@@ -13,7 +13,7 @@ foreach my $task (@tasks) {
 	push @{$task->{commands}}, (
 	
 		"seqtk seq -a ".$task->{inputfile}." > input.fasta",
-		"run_abundance.py -f input.fasta -c /root/.sepp/tipp.config -o .",
+		"run_abundance.py -f input.fasta -c /biobox/src/sepp/.sepp/tipp.config -o .",
 		"cp abundance.species.csv ".$task->{resultfilename}.".orig",
 		"perl -I ".$ENV{PREFIX}."/lib/ ".$ENV{PREFIX}."/bin/convert.pl ".$task->{resultfilename}.".orig ".$task->{taxonomyDir}." \"".$task->{inputfile}."\" > ".$task->{resultfilename}.".profile",
 		
